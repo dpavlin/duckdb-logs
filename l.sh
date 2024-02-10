2a9df43c8f7727689441e84399a50494af104d06
Dobrica Pavlinusic <dpavlin@rot13.org>
2024-02-10 15:00:15 +0100

    add filename

--- l.sh
+++ l.sh
@@ -2,14 +2,11 @@
 
 # import log files into duckdb
 
-rm logs.duckdb || true
-cat l.sql | ./duckdb logs.duckdb
-
 log2duckdb() {
 	logfile=$1
 	site=$( echo $logfile | sed -e 's,^.*/nginx/,,' -e 's/-*access-*//' -e 's/\.log.*$//'  -e 's/-ffzg-hr//')
 	echo "# $site $logfile"
-	echo "insert into nginx select '$site' as site,column0 as ip,column3 as ts,column5 as url,column6 as st,column7 as len,column8 as ref,column9 as ua from read_csv_auto('$logfile',delim=' ', ignore_errors=true,dateformat='%d/%b/%Y',timestampformat='[%d/%b/%Y:%H:%M:%S',filename=true);" | ./duckdb logs.duckdb || true
+	echo "insert into nginx select '$site' as site,column0 as ip,column3 as ts,column5 as url,column6 as st,column7 as len,column8 as ref,column9 as ua,filename from read_csv_auto('$logfile',delim=' ', ignore_errors=true,dateformat='%d/%b/%Y',timestampformat='[%d/%b/%Y:%H:%M:%S',filename=true);" | ./duckdb logs.duckdb || true
 }
 
 import_logs() {
@@ -25,9 +22,13 @@ if [ ! -z "$1" ] ; then
 	exit
 fi
 
+rm logs.duckdb || true
+cat l.sql | ./duckdb logs.duckdb
+
 import_logs /zamd/cluster/ws1.net.ffzg.hr/1/log/nginx/
 import_logs /zamd/oscar/ws2/0/var/log/nginx/
 import_logs /zamd/oscar/ws3/0/var/log/nginx/
+import_logs /zamd/cluster/pauk.ffzg.hr/0/var/log/nginx/
 
 exit 1
 
--- l.sql
+++ l.sql
@@ -5,7 +5,7 @@
 --- [09/Feb/2024:00:00:03 +0100]
 -- https://duckdb.org/docs/sql/functions/dateformat
 create or replace table nginx as
-select 'tzk' as site,column0 as ip,column3 as ts,column5 as url,column6 as st,column7 as len,column8 as ref,column9 as ua from read_csv_auto('/zamd/cluster/ws1.net.ffzg.hr/1/log/nginx/access-tzk-ffzg-hr.log',delim=' ',ignore_errors=true,dateformat='%d/%b/%Y',timestampformat='[%d/%b/%Y:%H:%M:%S',filename=true);
+select 'tzk' as site,column0 as ip,column3 as ts,column5 as url,column6 as st,column7 as len,column8 as ref,column9 as ua,filename from read_csv_auto('/zamd/cluster/ws1.net.ffzg.hr/1/log/nginx/access-tzk-ffzg-hr.log',delim=' ',ignore_errors=true,dateformat='%d/%b/%Y',timestampformat='[%d/%b/%Y:%H:%M:%S',filename=true);
 
 -- select count(ip),ip,min(st),max(st),sum(len),ref from nginx group by all order by count(ip) desc limit 10 ;
 
