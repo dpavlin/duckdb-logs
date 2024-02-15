c8e0142b4584218bea0453c1ee0d5c130d27e32f
Dobrica Pavlinusic <dpavlin@rot13.org>
2024-02-15 22:13:26 +0100

    incremental zeek log import

--- zeek2duckdb.sh
+++ zeek2duckdb.sh
@@ -1,25 +1,32 @@
-#!/bin/sh -e
+#!/bin/sh
 
 DB=zeek.duckdb
 what="create or replace table c as"
+test -e $DB && what="insert into c"
 
 duckdb() {
 	echo $@ | ./duckdb $DB
 }
 
-find zeek/2024-02-15 -name 'conn.*' | while read file ; do
+ls zeek/2024-02-15/conn.* | while read file ; do
 
-found=$( ./duckdb -csv --noheader zeek.duckdb "select count(*) as found from c where filename = '$file' ;" )
+	found=$( test -f $DB && ./duckdb -csv --noheader $DB "select count(*) as found from c where filename = '$file' ;" | tail -1 | grep -v '^\.' )
+	echo "XXX [$found] ###"
 
-	if [ "$found" = 0 ] ; then
+	test -z "$found" && found=0
 
-echo "$what select * \
-from read_csv('$file',ignore_errors=true,skip=8,filename=true,names=[ts,uid,orig_h,orig_p,resp_h,resp_p,proto,service,duration,orig_bytes,resp_bytes,conn_state,local_orig,local_resp,missed_bytes,history,orig_pkts,orig_ip_bytes,resp_pkts,resp_ip_bytes,tunnel_parents,vlan,inner_vlan,orig_l2_addr,resp_l2_addr]); \
+	if [ "$found" = "0" ] ; then
+
+		echo "$what select * \
+from read_csv('$file',ignore_errors=true,skip=8,nullstr='-',filename=true,names=[ts,uid,orig_h,orig_p,resp_h,resp_p,proto,service,duration,orig_bytes,resp_bytes,conn_state,local_orig,local_resp,missed_bytes,history,orig_pkts,orig_ip_bytes,resp_pkts,resp_ip_bytes,tunnel_parents,vlan,inner_vlan,orig_l2_addr,resp_l2_addr]); \
 " | ./duckdb $DB
-what="insert into c"
+
+		what="insert into c"
 
 	else
-		echo "SKIP $file"
+		echo "SKIP $file [$found]"
 	fi
 
 done
+
+./duckdb $DB "copy c to 'zeek-c.parquet'"
