02014be2b7816f6362839191da5f8eb090d1047d
Dobrica Pavlinusic <dpavlin@rot13.org>
2024-02-16 21:24:00 +0100

    remove ts conversion

--- zeek-attach.sh
+++ zeek-attach.sh
@@ -6,5 +6,5 @@ dates=$( ls zeek-*.duckdb | sed "s/^zeek-\(.*\).duckdb/\1/" | sed -e 's/^/select
 	| tr -d '\n' | sed 's/.c union $/.c ;/' )
 echo "create view c as $dates ;"
 
-echo "select orig_l2_addr, count(*) as c, sum(orig_ip_bytes) as o_b, sum(resp_ip_bytes) as r_b, to_timestamp(min(ts)::int) as from, to_timestamp(max(ts)) as to,min(vlan) from c where vlan is not null and vlan != 2 and to_timestamp(ts) > '2024-01-01' group by orig_l2_addr order by o_b;"
+echo "select orig_l2_addr, count(*) as c, sum(orig_ip_bytes) as o_b, sum(resp_ip_bytes) as r_b, min(ts), max(ts),min(vlan) from c where vlan is not null and vlan != 2 and ts > '2024-01-01' group by orig_l2_addr order by o_b;"
 
