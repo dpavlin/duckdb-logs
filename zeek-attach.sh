c4edd97e7df41f06a33fb3dfc071085287bd67d4
Dobrica Pavlinusic <dpavlin@rot13.org>
2024-02-17 09:36:20 +0100

    time_bucket and bar examples

--- zeek-attach.sh
+++ zeek-attach.sh
@@ -7,4 +7,6 @@ dates=$( ls zeek-*.duckdb | sed "s/^zeek-\(.*\).duckdb/\1/" | sed -e 's/^/select
 echo "create view c as $dates ;"
 
 echo "select orig_l2_addr, count(*) as c, sum(orig_ip_bytes) as o_b, sum(resp_ip_bytes) as r_b, min(ts), max(ts),min(vlan) from c where vlan is not null and vlan != 2 and ts > '2024-01-01' group by orig_l2_addr order by o_b;"
+echo "select count(*) as c, time_bucket('1 hour',ts) as t from c GROUP by t order by t ;"
+echo "select time_bucket('1 hour',ts) as t, count(*), bar(count(*),10000,1000000) as c from c GROUP by t order by t ;"
 
